f621f1a4fd906c5e8b06466d1ccf9e6054b6d3ae
Marek_Fryger <marekfryger@o2.pl>
2022-04-27 13:20:29 +0200

    match results page

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
 (function () {
     'use strict';
 
-    angular.module('NameCalculator', [])
+    angular.module('MatchResults', [])
+        .controller('MatchResultsController', MatchResultsController)
+        .constant('ServerAddress',"http://localhost:8080/")
+        .controller('MatchUpdate' ,['$scope',"$http",'ServerAddress',function ($scope, $http,ServerAddress) {
+            $scope.getUpdate = function () {
+                let date = new Date().toLocaleDateString();
+                let parsedDate=date.substring(6,10)+'-'+date.substring(3,5)+'-'+date.substring(0,2);
+                console.log(parsedDate);
+                $http({
+                    method: "GET",
+                    url: (ServerAddress+"matches/"+parsedDate)
+                });
+            }
+        }])
+        .service('MatchService', MatchService);
 
-        .controller('NameCalculatorController', function ($scope) {
-            $scope.name = "";
-            $scope.totalValue = 0;
 
-            $scope.displayNumeric = function () {
-                var totalNameValue = 0;// get the total value
-                $scope.totalValue = calculateNumericForString($scope.name);
-            };
-        });
+    MatchResultsController.$injected = ['MatchService'];
 
-    function calculateNumericForString(string) {
-        var totalStringValue = 0;
-        for (var i = 0; i < string.length; i++) {
-            totalStringValue += string.charCodeAt(i);
-        }
-        return totalStringValue;
+    function MatchResultsController(MatchService) {
+        let match = this;
+        let promise = MatchService.getMatchService();
+
+
+
+        promise.then(function (response) {
+            match.results = response.data;
+        })
+            .catch(function (error) {
+                console.log('Some error');
+            })
     }
+
+    MatchService.$inject = ['$http']
+
+    function MatchService($http) {
+        let service = this;
+
+        service.getMatchService = function () {
+            let response = $http({
+                method: "GET",
+                url: ("http://localhost:8080/matches")
+            });
+            return response;
+        }
+    }
+
 
 })();
