db8145d2da1308a2790e22637f7b75967a540fa2
Marek_Fryger <marekfryger@o2.pl>
2022-04-30 12:42:29 +0200

    style.css change

--- app.js
+++ app.js
@@ -3,16 +3,45 @@
 
     angular.module('MatchResults', [])
         .controller('MatchResultsController', MatchResultsController)
-        .constant('ServerAddress',"http://localhost:8080/")
-        .controller('MatchUpdate' ,['$scope',"$http",'ServerAddress',function ($scope, $http,ServerAddress) {
+        .constant('ServerAddress', "http://localhost:8080/")
+        .controller('MatchUpdate', ['$scope', "$http", 'ServerAddress', function ($scope, $http, ServerAddress) {
             $scope.getUpdate = function () {
                 let date = new Date().toLocaleDateString();
-                let parsedDate=date.substring(6,10)+'-'+date.substring(3,5)+'-'+date.substring(0,2);
+                let parsedDate = date.substring(6, 10) + '-' + date.substring(3, 5) + '-' + date.substring(0, 2);
                 console.log(parsedDate);
                 $http({
                     method: "GET",
-                    url: (ServerAddress+"matches/"+parsedDate)
+                    url: (ServerAddress + "matches/" + parsedDate)
                 });
+            }
+        }])
+        .controller('LeagueList',['$scope',function ($scope){
+            let leagues= [
+                {
+                    leagueName: 'Poland Ekstraklasa',
+                    leagueId: 'PL1'
+                }, {
+                    leagueName: 'England Premier League',
+                    leagueId: 'GB1'
+                }, {
+                    leagueName: 'Spanish La Liga',
+                    leagueId: 'ES1'
+                }, {
+                    leagueName: 'German Bundesliga',
+                    leagueId: 'L1'
+                }, {
+                    leagueName: 'Italy Serie A',
+                    leagueId: 'IT1'
+                }, {
+                    leagueName: 'France Ligue 1',
+                    leagueId: 'FR1'
+                }, {
+                    leagueName: 'Holland Eredivisie',
+                    leagueId: 'NL1'
+                },
+            ];
+            $scope.getLeagueList =function (){
+                return leagues;
             }
         }])
         .service('MatchService', MatchService);
@@ -20,7 +49,7 @@
 
     MatchResultsController.$injected = ['MatchService'];
 
-    function MatchResultsController(MatchService) {
+    function MatchResultsController(MatchService ) {
         let match = this;
         let promise = MatchService.getMatchService();
 
@@ -39,6 +68,7 @@
     function MatchService($http) {
         let service = this;
 
+
         service.getMatchService = function () {
             let response = $http({
                 method: "GET",
@@ -47,6 +77,9 @@
             return response;
         }
     }
+
+
+
 
 
 })();
